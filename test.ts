84bd75ad5ed8ff86e44913acd5eff5f8263314e9
feng <37255449+Uniquemf@users.noreply.github.com>
2021-10-04 15:49:39 +0800

    更新 pxt.json, main.ts, test.ts

--- test.ts
+++ test.ts
@@ -1,4 +1,5 @@
-Sentry.Begin(SentryId.Sentry00, sentry_mode_e.kI2CMode)
+Sentry.Begin(SentryId.Sentry00, sentry_mode_e.kSerialMode)
+Sentry.VisionSetStatus(SentryId.Sentry00, SentryStatus.Enable, sentry_vision_e.kVisionColor)
 basic.forever(function () {
     serial.writeNumber(Sentry.Cols(SentryId.Sentry00))
     serial.writeLine("")
