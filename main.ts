7d34fb923d682755b6ea39c3867ec822a42cb0a9
feng <37255449+Crafter-feng@users.noreply.github.com>
2024-11-30 17:20:45 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,1 @@
-serial.redirect(
-SerialPin.P14,
-SerialPin.P13,
-BaudRate.BaudRate115200
-)
-Sentry2VisionSensor.Begin(sentry_mode_e.kSerialMode, sentry2_addr_e.ADDR1)
-Sentry2VisionSensor.CameraSetAwb(sentry_camera_white_balance_e.kAutoWhiteBalance)
-Sentry2VisionSensor.VisionSetStatus(sentry2_status.Enable, sentry_vision_e.kVisionLine)
-basic.forever(function () {
-    basic.showNumber(Sentry2VisionSensor.Detected(sentry_vision_e.kVisionLine))
-})
+
